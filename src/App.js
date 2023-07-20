885f17a13b6fb38f74116f0e317284ae7801ad81
Jonas Kaatz <jonas@jonaskaatz.com>
2023-07-20 16:12:55 +0200

    make client working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,124 @@
-import React from 'react';
+import React,{useMemo,useState,useEffect} from 'react';
 import './App.css';
 import banner from './banner.png'; 
 import Navbar from './Navbar';
+import { MaterialReactTable } from 'material-react-table'; // https://www.material-react-table.com/docs/examples/basic
+
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ojyikcluxfqbxcpacdvy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeWlrY2x1eGZxYnhjcGFjZHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5ODY4MDcsImV4cCI6MjAwNDU2MjgwN30.VBBL_CvwWLfnMyo1ZgOOCXf9B4degD2qDcNAtHoDj7I';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// https://supabase.com/docs/guides/functions/examples/stripe-webhooks
+// const URL='https://feedback-forms-backend.vercel.app';
+
+const dataTable=[
+    {
+        name:'John Doe',
+        address:'261 Erdman Ford',
+        city:'East Daphne',
+        state:'Kentucky',
+    },
+    {
+        name:'Jane Doe',
+        address:'769 Dominic Grove',
+        city:'Columbus',
+        state:'Ohio',
+    },
+    {
+        name:'Joe Doe',
+        address:'566 Brakus Inlet',
+        city:'South Linda',
+        state:'West Virginia',
+    },
+    {
+        name:'Kevin Vandy',
+        address:'722 Emie Stream',
+        city:'Lincoln',
+        state:'Nebraska',
+    },
+    {
+        name:'Joshua Rolluffs',
+        address:'32188 Larkin Turnpike',
+        city:'Charleston',
+        state:'South Carolina',
+    },
+];
+
+// const { data: { user } } = await supabase.auth.getUser()
+
+
 
 function App() {
-    // async function submitForm() {
-    //     let formData={
-    //         api_key:'233b3086-8b9e-4ed8-a72c-8c962ead1461',
-    //         name:"John Doe",
-    //         email:"john-doe@example.com",
-    //         // test:"hallo das ist ein test"
-    //     };
-    //     let response=await fetch('http://192.168.2.47:3001/forms', {
-    //         method:'POST',
-    //         headers:{
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body:JSON.stringify(formData),
-    //     });
-    //     let json=await response.json();
-    //     alert(JSON.stringify(json))
-    // };
+    const [login,setLogin]=useState(false);
+    supabase.auth.onAuthStateChange((event, session) => {
+        if (session) {
+            setLogin(true);
+        }
+        else setLogin(false);
+        // console.log(event, session)
+    })
+
+    async function submitForm() {
+        let formData={
+            api_key:'233b3086-8b9e-4ed8-a72c-8c962ead1461',
+            name:"John Doe",
+            email:"john-doe@example.com",
+            // test:"hallo das ist ein test"
+        };
+        let response=await fetch(URL+'/forms', {
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(formData),
+        });
+        let json=await response.json();
+        console.log(JSON.stringify(json))
+    };
+    const columns=useMemo(()=>[
+        {
+            accessorKey:'name',
+            header:'Name',
+            size:150,
+        },
+        {
+            accessorKey:'address',
+            header:'Address',
+            size:200,
+        },
+        {
+            accessorKey:'city',
+            header:'City',
+            size:150,
+        },
+        {
+            accessorKey:'state',
+            header:'State',
+            size:150,
+        },
+    ],[]);
+
+    async function checkUser() {
+        let { data: users, error } = await supabase
+            .from('users')
+            .select()
+        console.log(users);
+    }
 
     return (
         <div className="App">
             {/* <button onClick={submitForm}>Submit</button> */}
-            <div className="flex flex-col items-center bg-gradient-to-br from-indigo-500 to-purple-500 h-screen">
-                <Navbar/>
+            <button onClick={checkUser}>user</button>
+            <div className="flex flex-col items-center bg-gradient-to-br from-indigo-500 to-purple-500 h-s96">
+                <Navbar supabase={supabase} login={login}/>
                 <h1 className="text-7xl text-white">Form feedback</h1>
-                <img src={banner} alt="banner" />
+                <img className="m-9" src={banner} alt="banner" />
             </div>
+            <MaterialReactTable 
+                columns={columns} 
+                data={dataTable} 
+            />
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,63 @@
-import {React} from 'react'
+import {React, useState} from 'react';
+import Modal from 'react-modal';
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-export default function Navbar({handleLangChange,handleNavChange,isOpen,setIsOpen}) {
+export default function Navbar({supabase,login}) {
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+
+    function openModal() {setIsOpen(true);}
+    // function handleEmailChange(event) {setUserEmail(event.target.value);}
+    
+    async function signOut() {
+        const { error } = await supabase.auth.signOut()
+    }
+
+    function closeModal() {setIsOpen(false);setUserEmail('');}
+
+    // async function loginUser() {
+    //     // let { data, error } = await supabase.auth.signInWithOtp({
+    //     //     email: userEmail
+    //     // })
+    //     let { data, error } = await supabase.auth.signInWithOtp({
+    //         email: 'jonas.kaatz95@gmail.com'
+    //     })
+    //     console.log(data);
+    //     console.log(error);
+    // }
     return (
-        <nav className="flex flex-col items-center w-screen bg-white">
+        <nav className="flex flex-col items-center w-full bg-white">
             <div className="w-full md:w-3/4 lg:w-2/3 flex items-center justify-between h-16 px-4 max-w-7xl">
                 <span className="text-whiste font-bold text-xl">welcome</span>
                 <div className="flex flex-row items-center py-2">
-                    <button className="flex justify-center items-center py-2 w-24 text-center bg-indigo-500 rounded-lg text-white">test</button>
+                    {login?
+                    <button className="flex justify-center items-center py-2 w-24 text-center bg-indigo-500 rounded-lg text-white" onClick={signOut}>logout</button>
+                    :
+                    <button className="flex justify-center items-center py-2 w-24 text-center bg-indigo-500 rounded-lg text-white" onClick={openModal}>Login</button>
+                    }
                 </div>
             </div>
-            {/* modal */}
+            <Modal isOpen={modalIsOpen} ariaHideApp={false} className={'h-1/2 flex items-center justify-center'}>
+                <div className="w-11/12 max-w-lg h-s48 bg-slate-100 p-2 rounded-xl shadow-lg shadow-slate-400">
+                    <div className="flex justify-between mb-3">
+                        <h2 className="text-4xl">Login</h2>
+                        <button onClick={closeModal}>X</button>
+                    </div>
+                    <div className="p-4">
+                        <Auth
+                            supabaseClient={supabase}
+                            appearance={{ theme: ThemeSupa }}
+                            theme="dark"
+                            providers={false}
+                            className={"w-96 bg-blue-200"}
+                            // providers={["google", "facebook", "github"]}
+                        />
+                        {/* <input className="px-4 py-2 rounded-xl border" type='text' value={userEmail} onChange={handleEmailChange} placeholder='Email'/> */}
+                        {/* <button className="flex justify-center items-center py-2 w-24 text-center bg-indigo-500 rounded-lg text-white" onClick={loginUser}>Login</button> */}
+                    </div>
+                </div>
+            </Modal>
         </nav>
     )
 }
